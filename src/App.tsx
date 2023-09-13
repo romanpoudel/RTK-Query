d21b2b31243a53087c98cdf4a51b277bd5fcb79a
romanpoudel <romanpoudel325@gmail.com>
2023-09-13 17:13:29 +0545

    finish CRUD operation

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,84 @@
-import './App.css'
-import { useContactQuery, useContactsQuery } from './services/contactsApi'
+import "./App.css";
+import {
+	useAddContactMutation,
+	useContactQuery,
+	useContactsQuery,
+  useDeleteContactMutation,
+  useUpdateContactMutation,
+} from "./services/contactsApi";
 
 function App() {
-const {data,error,isLoading,isFetching,isSuccess}=useContactsQuery();
+	const { data, error, isLoading, isFetching, isSuccess } =
+		useContactsQuery();
 
-  return (
-    <div className='App'>
-      <h1>React Redux Toolkit RTK Query Practice</h1>
-      {isLoading && <div>Loading...</div>}
-      {isFetching && <div>Fetching...</div>}
-      {error && <div>Something went wrong</div>}
-      {isSuccess && (
-        <div>
-          <h2>Contacts</h2>
-            {data?.map((contact) => (
-              <div className='data' key={contact.id}>
-                <span>{contact.name}</span>
-                <span><ContactDetail id={contact.id}/></span>
-              </div>
-            ))}
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>React Redux Toolkit RTK Query Practice</h1>
+			{isLoading && <div>Loading...</div>}
+			{isFetching && <div>Fetching...</div>}
+			{error && <div>Something went wrong</div>}
+			{isSuccess && (
+				<div>
+					<h2>Contacts</h2>
+					{data?.map((contact) => (
+						<div className="data" key={contact.id}>
+							<span>{contact.name}</span>
+							<span>
+								<ContactDetail id={contact.id} />
+							</span>
+						</div>
+					))}
+				</div>
+			)}
+      <div>
+        <AddContact/>
+      </div>
+		</div>
+	);
 }
 
-const ContactDetail=({id}:{id:string})=>{
-  const {data}=useContactQuery(id); 
-  return (
-    <pre>{JSON.stringify(data,undefined,2)}</pre>
-  )
-}
+const ContactDetail = ({ id }: { id: string }) => {
+	const { data } = useContactQuery(id);
+	return <pre>{JSON.stringify(data, undefined, 2)}</pre>;
+};
 
+const AddContact = () => {
+	const [addContact] = useAddContactMutation();
+  const [updateContact] = useUpdateContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
+  //for manual refresh after update in db
+  // const {refetch}=useContactsQuery();
+	const contact = {
+		id: "10",
+		name: "John Doe",
+		email: "john@gmail.com",
+	};
 
-export default App
+	const contactUpdate = {
+		id: "10",
+		name: "John Doe Updated",
+		email: "john@gmail.com",
+	};
+
+  const addHandler=async()=>{
+    await addContact(contact);
+    // refetch();
+  }
+  const updateHandler=async()=>{
+    await updateContact(contactUpdate);
+    // refetch();
+  }
+  const deleteHandler=async()=>{
+    await deleteContact(contact.id);
+    // refetch();
+  }
+	return (
+		<div>
+			<h2>Add Contact</h2>
+      <button type="button" onClick={addHandler}>Add Contact</button>
+      <button type="button" onClick={updateHandler}>Update Contact</button>
+      <button type="button" onClick={deleteHandler}>Delete Contact</button>
+		</div>
+	);
+};
+export default App;
--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -4,14 +4,40 @@ import {Contact} from '../models/contact.model';
 export const contactsApi=createApi({
     reducerPath:'contactsApi',
     baseQuery:fetchBaseQuery({baseUrl:'http://localhost:3000/'}),
+    tagTypes:['Contact'],
     endpoints:(builder)=>({
         contacts:builder.query<Contact[],void>({
             query:()=>"contacts",
+            providesTags:['Contact'],
         }),
         contact:builder.query<Contact,string>({
             query:(id)=>`contacts/${id}`,
+            providesTags:['Contact'],
+        }),
+        addContact:builder.mutation<void,Contact>({
+            query:(contact)=>({
+                url:'contacts',
+                method:'POST',
+                body:contact,
+            }),
+            invalidatesTags:['Contact'],
+        }),
+        updateContact:builder.mutation<void,Contact>({
+            query:({id,...rest})=>({
+                url:`contacts/${id}`,
+                method:'PUT',
+                body:rest,
+            }),
+            invalidatesTags:['Contact'],
+        }),
+        deleteContact:builder.mutation<void,string>({
+            query:(id)=>({
+                url:`contacts/${id}`,
+                method:'DELETE',
+            }),
+            invalidatesTags:['Contact'],
         }),
     }),
 });
 
-export const {useContactsQuery,useContactQuery}=contactsApi;
+export const {useContactsQuery,useContactQuery,useAddContactMutation,useDeleteContactMutation,useUpdateContactMutation}=contactsApi;
